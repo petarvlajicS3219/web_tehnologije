a7867c689eb42f84e27ee23cbc6fec7a910f0cf2
petarvlajics3219 <vlajic.p27@gmail.com>
2020-10-13 15:30:02 +0200

    Domaci1

---
+++ Domaci1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    echo "Ime:Petar
+        <br>
+            Prezime:Vlajic
+        <br>
+        Broj indeksa: s32/19
+         "
+    ?>
+</body>
+</html>
